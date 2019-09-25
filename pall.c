1f7c23ad3757c7b3950481d41f79de13670c0cbb
Michael <michael.sosa@holbertonschool.com>
2019-09-25 07:29:32 -0500

    [Update] I implemented the global struck, and removed some blank spaces

--- pall.c
+++ pall.c
@@ -2,5 +2,5 @@
 
 void pall(stack_t **stack, unsigned int line_cnt __attribute__((unused)))
 {
-	print_stack(*stack);		
+	print_stack(*stack);
 }
--- push.c
+++ push.c
@@ -5,15 +5,13 @@
 
 void push(stack_t **stack, unsigned int line_cnt)
 {
-	char *n = strtok(NULL, " \t\n");
-
-	if (isnumber(n))
+	if (isnumber(global.argument))
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_cnt);
 		exit(EXIT_FAILURE);
 	}
 
-	if (!add_node(stack, atoi(n)))
+	if (!add_node(stack, atoi(global.argument)))
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
