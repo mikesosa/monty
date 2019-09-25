3315b05c7ae6d6caa96494c02704b73001580137
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-09-25 17:33:41 -0500

    (fix) segfault with opcode with no argument

--- push.c
+++ push.c
@@ -8,15 +8,17 @@ void push(stack_t **stack, unsigned int line_cnt)
 {
 	char *n = global.argument;
 
-	if (!isdigit(*n))
+	if (!n || !isdigit(*n))
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_cnt);
 		status = EXIT_FAILURE;
+		return;
 	}
 
 	if (!add_node(stack, atoi(n)))
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		status = EXIT_FAILURE;
+		return;
 	}
 }
