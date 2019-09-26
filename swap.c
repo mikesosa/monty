b4a78959d73929b298cada678732d8d30f59d32f
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-09-25 19:26:35 -0500

    (fix) pedantic on function swap

--- swap.c
+++ swap.c
@@ -12,14 +12,18 @@
  */
 void swap(stack_t **stack, unsigned int line_cnt)
 {
+	int top;
+	int prev;
+
 	if (!stack || !(*stack) || !(*stack)->next)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_cnt);
 		exit(EXIT_FAILURE);
 	}
-	int top = (*stack)->n;
-	int before = (*stack)->next->n;
+
+	top = (*stack)->n;
+	prev = (*stack)->next->n;
 
 	(*stack)->next->n = top;
-	(*stack)->n = before;
+	(*stack)->n = prev;
 }
