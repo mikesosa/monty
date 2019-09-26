6acc810184d7e69922a6bbd7f31aeadd7b55f49b
Michael <michael.sosa@holbertonschool.com>
2019-09-25 19:14:33 -0500

    [Update] Betty styling

--- swap.c
+++ swap.c
@@ -3,9 +3,16 @@
 #include <string.h>
 #include "monty.h"
 
+/**
+ * swap -  swaps data from top to previous
+ * @stack: stack given by main
+ * @line_cnt: ammount of lines
+ *
+ * Return: void
+ */
 void swap(stack_t **stack, unsigned int line_cnt)
 {
-	if(!stack || !(*stack) || !(*stack)->next)
+	if (!stack || !(*stack) || !(*stack)->next)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_cnt);
 		exit(EXIT_FAILURE);
