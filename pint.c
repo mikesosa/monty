cb379ee3dda878ffbcd6686db061e22e1e1a93fc
Michael <michael.sosa@holbertonschool.com>
2019-09-25 19:56:32 -0500

    [Fix] memory leak when error on pint

--- pint.c
+++ pint.c
@@ -15,7 +15,8 @@ void pint(stack_t **stack, unsigned int line_cnt)
 	if (!stack || !(*stack))
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_cnt);
-		exit(EXIT_FAILURE);
+		status = EXIT_FAILURE;
+		return;
 	}
 	printf("%d\n", (*stack)->n);
 }
