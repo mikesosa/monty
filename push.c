d8185244f8ac98f7b2549f02f5aa69fc7962b234
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-09-26 21:59:11 -0500

    (refactor) push

--- push.c
+++ push.c
@@ -23,13 +23,14 @@ void push(stack_t **stack, unsigned int line_cnt)
 	}
 
 	if (global.data_struct == 1)
-	{	
+	{
 		if (!add_node(stack, atoi(global.argument)))
 		{
 			return;
 			status = EXIT_FAILURE;
 		}
-	}else
+	}
+	else
 	{
 		if (!queue_node(stack, atoi(global.argument)))
 		{
